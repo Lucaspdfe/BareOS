52a3993bfbc850968490e09ae134427c68a975eb
Lucas <lucaspdfe@gmail.com>
2025-11-20 18:57:50 -0300

    Added a way to compile binutils and gcc.

--- BUILD.md
+++ BUILD.md
@@ -2,11 +2,35 @@
 
 ## Dependencies
 
+You'll need to use Linux, preferably any debian-based distro, or you can use WSL(Windows Subsystem for Linux) for Windows.
+
 List of dependencies:
     `make`
     `nasm`
     `mtools`
     `qemu-system`
+    `build-essential`
+    `bison`
+    `flex`
+    `libgmp-dev`
+    `libmpfr-dev`
+    `libmpc-dev`
+    `texinfo`
+    `wget`
+    `xz-utils`
+
+### Toolchain (i686-elf)
+
+To compile the toolchain, run these commands:
+
+```bash
+./toolchain.sh fetch_binutils
+./toolchain.sh fetch_gcc
+./toolchain.sh toolchain
+echo 'export PATH="$PATH:/opt/i686-elf-toolchain/i686-elf/bin"' >> ~/.bashrc
+   ```
+
+(If this command didn't work, try modifying $BINUTILS_VERSION and $GCC_VERSION on toolchain.sh)
 
 ## Compiling
 
---
+++ toolchain.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+# Fixed install path
+TOOLCHAIN_PREFIX="/opt/i686-elf-toolchain/i686-elf"
+export PATH="$TOOLCHAIN_PREFIX/bin:$PATH"
+
+BINUTILS_VERSION=2.42
+GCC_VERSION=13.3.0
+
+# Variables that still depend on environment
+BINUTILS_SRC="toolchain/binutils-$BINUTILS_VERSION"
+BINUTILS_BUILD="toolchain/binutils-build-$BINUTILS_VERSION"
+
+GCC_SRC="toolchain/gcc-$GCC_VERSION"
+GCC_BUILD="toolchain/gcc-build-$GCC_VERSION"
+
+BINUTILS_URL="https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz"
+GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz"
+
+#----------------------------------------
+# Binutils
+#----------------------------------------
+
+fetch_binutils() {
+    mkdir -p toolchain
+    cd toolchain && wget -nc "$BINUTILS_URL"
+}
+
+toolchain_binutils() {
+    tar -xf "toolchain/binutils-$BINUTILS_VERSION.tar.xz" -C "toolchain/"
+    mkdir -p "$BINUTILS_BUILD"
+    cd "$BINUTILS_BUILD" && ../"binutils-$BINUTILS_VERSION"/configure \
+        --prefix="$TOOLCHAIN_PREFIX" \
+        --target="i686-elf" \
+        --with-sysroot \
+        --disable-nls \
+        --disable-werror
+
+    cd ../..
+    make -j8 -C "$BINUTILS_BUILD"
+    sudo make -C "$BINUTILS_BUILD" install
+}
+
+#----------------------------------------
+# GCC
+#----------------------------------------
+
+fetch_gcc() {
+    mkdir -p toolchain
+    cd toolchain && wget -nc "$GCC_URL"
+}
+
+toolchain_gcc() {
+    toolchain_binutils
+    tar -xf "gcc-$GCC_VERSION.tar.xz" -C "toolchain/"
+    mkdir -p "$GCC_BUILD"
+    cd "$GCC_BUILD" && ../"gcc-$GCC_VERSION"/configure \
+        --prefix="$TOOLCHAIN_PREFIX" \
+        --target="i686-elf" \
+        --disable-nls \
+        --enable-languages=c,c++ \
+        --without-headers
+    cd ../..
+    make -j8 -C "$GCC_BUILD" all-gcc all-target-libgcc
+    sudo make -C "$GCC_BUILD" install-gcc install-target-libgcc
+}
+
+#----------------------------------------
+# Cleaning
+#----------------------------------------
+
+clean_toolchain() {
+    rm -rf "$GCC_BUILD" "$GCC_SRC" "$BINUTILS_BUILD" "$BINUTILS_SRC"
+}
+
+clean_toolchain_all() {
+    rm -rf toolchain/*
+}
+
+#----------------------------------------
+# Dispatch
+#----------------------------------------
+
+case "$1" in
+    toolchain)
+        toolchain_binutils
+        toolchain_gcc
+        ;;
+    toolchain_binutils)
+        toolchain_binutils
+        ;;
+    toolchain_gcc)
+        toolchain_gcc
+        ;;
+    fetch_binutils)
+        fetch_binutils
+        ;;
+    fetch_gcc)
+        fetch_gcc
+        ;;
+    clean-toolchain)
+        clean_toolchain
+        ;;
+    clean-toolchain-all)
+        clean_toolchain_all
+        ;;
+    *)
+        echo "Unknown command"
+        exit 1
+        ;;
+esac
